74252df44a0d6eb20ba41b8be789b85dea9fb386
AKM Rafiqul Alam <akmrafiq450@gmail.com>
2021-09-26 17:36:54 +0600

    LargestSubstringBetweenTwoEqualCharacters solved

---
+++ LeetCode-Problem-Solving/LeetCode-Problem-Solving/LargestSubstringBetweenTwoEqualCharacters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_Problem_solving
+{
+    public class LargestSubstringBetweenTwoEqualCharacters
+    {
+        public static int MaxLengthBetweenEqualCharacters(string s)
+        {
+            var result = -1;
+            var dictionary = new Dictionary<char, int>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (dictionary.ContainsKey(c))
+                    result = Math.Max(result, i - dictionary[c]);
+                else dictionary[c] = i;
+            }
+            return result == -1 ? -1 : result - 1;
+        }
+    }
+}
---
+++ LeetCode-Problem-Solving/LeetCode-Problem-Solving/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode_Problem_solving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "scayofdzycaz";
+            Console.WriteLine(LargestSubstringBetweenTwoEqualCharacters.MaxLengthBetweenEqualCharacters(s));
+        }
+    }
+}
